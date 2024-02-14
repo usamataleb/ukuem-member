0ca940af924e0c7fcdbe0ee1be53268934579e7f
usamataleb <mamenmasau@gmail.com>
2024-02-14 18:53:54 +0300

    Building Error Solved

--- rollup.config.js
+++ rollup.config.js
@@ -26,16 +26,13 @@ const aliases = alias({
 const indexTemplate = `<!--
 
 =========================================================
-* Notus Svelte - v1.1.0 based on Tailwind Starter Kit by Creative Tim
+* Notus Svelte - v1.1.0 based on Tailwind Starter Kit by Usama Talib Juma
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/notus-svelte
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/notus-svelte/blob/main/LICENSE.md)
+* Copyright 2021 Usama Talib Juma 
 
-* Tailwind Starter Kit Page: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
 
-* Coded by Creative Tim
+* Coded by Usama Talib Juma
 
 =========================================================
 
@@ -49,7 +46,7 @@ const indexTemplate = `<!--
     <meta name="viewport" content="width=device-width,initial-scale=1" />
 
     <link rel="shortcut icon" href="/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/ukuem.png" />
     <link rel="stylesheet" href="/build/bundle.css" />
     <link
       rel="stylesheet"
@@ -63,8 +60,8 @@ const indexTemplate = `<!--
         var process = { env: {<<process-env-status>>} };
       }
     </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"></script>
-    <script defer src="<<live-preview-link>>/build/bundle.js"></script>
+    <script src="build/bundle.js.map"></script>
+    <script defer src="build/bundle.js"></script>
   </head>
 
   <body class="text-blueGray-700 antialiased">
